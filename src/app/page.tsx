d004a6cce13a180c7d08a2f71df6f7d848001ec0
skatezza <jung.hyungshik@gmail.com>
2024-06-26 10:43:43 +0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Card } from "@mantine/core";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold">Hello world</h1>
+      <Card>Hello</Card>
     </main>
   );
 }
